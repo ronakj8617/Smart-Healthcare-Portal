979212c4ad782523711940fd259e2bb3a587f7fe
Ronak Parmar <ronakp447450@gmail.com>
2025-04-04 17:58:23 +0530

    Created the frontend and backend projects
    Signed-off-by: Ronak Parmar <ronakp447450@gmail.com>

---
+++ Smart-Healthcare-Portal-Backend/src/test/java/com/example/smarthealthcareportalbackend/SmartHealthcarePortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smarthealthcareportalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHealthcarePortalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
